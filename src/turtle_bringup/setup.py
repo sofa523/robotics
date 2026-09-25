cdd0ab8385dcdd4c735ae00f9af9274fce8bc878
sofa523 <s.markina@g.nsu.ru>
2026-09-25 14:38:43 +0700

    first

---
+++ src/turtle_bringup/setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+from glob import glob
+
+
+package_name = 'turtle_bringup'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', glob('launch/*.launch.py')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='root',
+    maintainer_email='s.markina@g.nsu.ru',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
